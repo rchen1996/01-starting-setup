0cc5ec52072eb12d694a53d56632661cdaea8d6e
Rebecca Chen <rebecca061996@gmail.com>
2021-05-31 16:19:50 -0400

    Comment out code for test button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -7,9 +7,9 @@ import './ExpenseItem.css';
 function ExpenseItem(props) {
   const [title, setTitle] = useState(props.title);
 
-  const clickHandler = () => {
-    setTitle('Updated!');
-  };
+  // const clickHandler = () => {
+  //   setTitle('Updated!');
+  // };
 
   return (
     <li>
@@ -19,7 +19,7 @@ function ExpenseItem(props) {
           <h2>{title}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
+        {/* <button onClick={clickHandler}>Change Title</button> */}
       </Card>
     </li>
   );
